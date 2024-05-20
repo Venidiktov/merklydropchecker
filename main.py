0be9fff558adffd8dd403ebcc23bbedcd4955fb6
Venidiktov <97854377+Venidiktov@users.noreply.github.com>
2024-05-20 12:29:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import os
 
+# Путь к директории и файлу adresses.txt
 current_directory = os.path.dirname(os.path.abspath(__file__))
 addresses_file_path = os.path.join(current_directory, 'adresses.txt')
 
+# Чтение адресов для сравнения и приведение их к нижнему регистру
 with open(addresses_file_path, 'r') as file:
-    addresses_to_compare = [line.strip() for line in file.readlines()]
+    addresses_to_compare = [line.strip().lower() for line in file.readlines()]
 
 address_points = {}
 
+# Обработка файлов с суффиксом points.txt
 for filename in os.listdir(current_directory):
     if filename.endswith('points.txt'):
         points = int(filename.split('points')[0])
@@ -15,14 +18,16 @@ for filename in os.listdir(current_directory):
 
         with open(filepath, 'r') as file:
             addresses = file.readlines()
+
             for address in addresses:
-                address = address.strip()
+                address = address.strip().lower()
                 if address in addresses_to_compare:
                     if address in address_points:
                         address_points[address] += points
                     else:
                         address_points[address] = points
 
+# Запись результатов в файл result.txt
 result_file_path = os.path.join(current_directory, 'result.txt')
 with open(result_file_path, 'w') as file:
     for address, points in address_points.items():
