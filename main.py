8d568bf985866c1d746664e3e2b16e919a4c2047
Venidiktov <97854377+Venidiktov@users.noreply.github.com>
2024-05-20 12:32:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from collections import defaultdict
 
 # Путь к директории и файлу adresses.txt
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -9,6 +10,7 @@ with open(addresses_file_path, 'r') as file:
     addresses_to_compare = [line.strip().lower() for line in file.readlines()]
 
 address_points = {}
+points_count = defaultdict(int)
 
 # Обработка файлов с суффиксом points.txt
 for filename in os.listdir(current_directory):
@@ -27,10 +29,18 @@ for filename in os.listdir(current_directory):
                     else:
                         address_points[address] = points
 
+# Подсчет количества кошельков для каждого количества поинтов
+for points in address_points.values():
+    points_count[points] += 1
+
 # Запись результатов в файл result.txt
 result_file_path = os.path.join(current_directory, 'result.txt')
 with open(result_file_path, 'w') as file:
     for address, points in address_points.items():
         file.write(f'{address}: {points}\n')
+
+    file.write('\nКоличество кошельков по количеству поинтов:\n')
+    for points, count in sorted(points_count.items()):
+        file.write(f'{points} points - {count} wallets\n')
 
 print("Все адреса успешно обработаны и сохранены в result.txt")
